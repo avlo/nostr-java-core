793423507eecf31735fb0e19a750527cff877927
nick avlo <avlo@yahoo.com>
2025-07-03 16:19:54 -0700

    added relaysTag to BadgeDefinition event (mainly out of client api convience)

--- src/main/java/com/prosilion/nostr/event/BadgeDefinitionEvent.java
+++ src/main/java/com/prosilion/nostr/event/BadgeDefinitionEvent.java
@@ -3,16 +3,27 @@ package com.prosilion.nostr.event;
 import com.prosilion.nostr.NostrException;
 import com.prosilion.nostr.enums.Kind;
 import com.prosilion.nostr.tag.IdentifierTag;
+import com.prosilion.nostr.tag.RelaysTag;
 import com.prosilion.nostr.user.Identity;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import lombok.Getter;
 import org.springframework.lang.NonNull;
 
 public class BadgeDefinitionEvent extends BaseEvent {
+  @Getter
+  private final IdentifierTag identifierTag;
+
+  @Getter
+  private final RelaysTag relaysTag;
+
   public BadgeDefinitionEvent(
       @NonNull Identity identity,
       @NonNull IdentifierTag identifierTag,
+      @NonNull RelaysTag relaysTag,
       @NonNull String content) throws NostrException, NoSuchAlgorithmException {
-    super(identity, Kind.BADGE_DEFINITION_EVENT, List.of(identifierTag), content);
+    super(identity, Kind.BADGE_DEFINITION_EVENT, List.of(identifierTag, relaysTag), content);
+    this.identifierTag = identifierTag;
+    this.relaysTag = relaysTag;
   }
 }
