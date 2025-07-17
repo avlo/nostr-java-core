0055a9523fe4b973ea5eabe725249d8799f2c2e4
nick avlo <avlo@yahoo.com>
2025-07-16 21:47:22 -0700

    gson remove

--- src/main/java/com/prosilion/nostr/filter/tag/ReferenceTagFilter.java
+++ src/main/java/com/prosilion/nostr/filter/tag/ReferenceTagFilter.java
@@ -5,7 +5,6 @@ import com.prosilion.nostr.event.GenericEventKindIF;
 import com.prosilion.nostr.filter.AbstractFilterable;
 import com.prosilion.nostr.filter.Filterable;
 import com.prosilion.nostr.tag.ReferenceTag;
-import java.net.URI;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import lombok.EqualsAndHashCode;
@@ -35,5 +34,5 @@ public class ReferenceTagFilter extends AbstractFilterable<ReferenceTag> {
     return super.getFilterable();
   }
 
-  public static Function<JsonNode, Filterable> fxn = node -> new ReferenceTagFilter(new ReferenceTag(URI.create(node.asText())));
+  public static Function<JsonNode, Filterable> fxn = node -> new ReferenceTagFilter(new ReferenceTag(node.asText()));
 }
