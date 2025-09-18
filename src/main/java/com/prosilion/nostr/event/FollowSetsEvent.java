cd95de4ba47ea3e8c05cb483adfd93008bde65f8
nick avlo <avlo@yahoo.com>
2025-09-18 15:54:52 +0800

    follow sets event up

--- src/main/java/com/prosilion/nostr/event/FollowSetsEvent.java
+++ src/main/java/com/prosilion/nostr/event/FollowSetsEvent.java
@@ -5,18 +5,24 @@ import com.prosilion.nostr.enums.Kind;
 import com.prosilion.nostr.tag.AddressTag;
 import com.prosilion.nostr.tag.BaseTag;
 import com.prosilion.nostr.tag.EventTag;
+import com.prosilion.nostr.tag.IdentifierTag;
 import com.prosilion.nostr.tag.PubKeyTag;
 import com.prosilion.nostr.user.Identity;
 import com.prosilion.nostr.user.PublicKey;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.springframework.lang.NonNull;
 
 public class FollowSetsEvent extends BaseEvent {
-  public FollowSetsEvent(@NonNull Identity identity, @NonNull PublicKey publicKey, @NonNull List<EventTagAddressTagPair> pairedTags, @NonNull String content) throws NostrException, NoSuchAlgorithmException {
+  public FollowSetsEvent(
+      @NonNull Identity identity,
+      @NonNull PublicKey publicKey,
+      @NonNull List<EventTagAddressTagPair> pairedTags,
+      @NonNull String content) throws NostrException, NoSuchAlgorithmException {
     super(
         identity,
         Kind.FOLLOW_SETS,
@@ -30,9 +36,67 @@ public class FollowSetsEvent extends BaseEvent {
         content);
   }
 
+  public FollowSetsEvent(
+      @NonNull Identity identity,
+      @NonNull PublicKey publicKey,
+      @NonNull IdentifierTag identifierTag,
+      @NonNull List<EventTagAddressTagPair> pairedTags,
+      @NonNull String content) throws NostrException, NoSuchAlgorithmException {
+    super(
+        identity,
+        Kind.FOLLOW_SETS,
+        Stream.concat(
+                Stream.concat(
+                    pairedTags.stream()
+                        .map(EventTagAddressTagPair::getTags)
+                        .flatMap(Collection::stream),
+                    Stream.of(
+                        new PubKeyTag(publicKey))),
+                Stream.of(identifierTag))
+            .collect(Collectors.toList()),
+        content);
+  }
+
+  public FollowSetsEvent(
+      @NonNull Identity identity,
+      @NonNull PublicKey publicKey,
+      @NonNull List<EventTagAddressTagPair> pairedTags,
+      @NonNull List<BaseTag> baseTags,
+      @NonNull String content) throws NostrException, NoSuchAlgorithmException {
+    super(
+        identity,
+        Kind.FOLLOW_SETS,
+        Stream.concat(
+                Stream.concat(
+                    pairedTags.stream()
+                        .map(EventTagAddressTagPair::getTags)
+                        .flatMap(Collection::stream),
+                    Stream.of(
+                        new PubKeyTag(publicKey))),
+                baseTags.stream()
+                    .filter(Predicate.not(EventTag.class::isInstance))
+                    .filter(Predicate.not(AddressTag.class::isInstance)))
+            .collect(Collectors.toList()),
+        content);
+  }
+
   public record EventTagAddressTagPair(@NonNull EventTag eventTag, @NonNull AddressTag addressTag) {
     public List<BaseTag> getTags() {
       return List.of(eventTag, addressTag);
     }
+
+//    @Override
+//    public boolean equals(Object o) {
+//      if (o == null || getClass() != o.getClass()) return false;
+//      EventTagAddressTagPair that = (EventTagAddressTagPair) o;
+//      boolean equals = Objects.equals(eventTag, that.eventTag);
+//      boolean equals1 = Objects.equals(addressTag, that.addressTag);
+//      return equals && equals1;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//      return Objects.hash(eventTag, addressTag);
+//    }
   }
 }
---
+++ src/test/java/com/prosilion/nostr/FollowSetsEventTest.java
@@ -0,0 +1,76 @@
+package com.prosilion.nostr;
+
+import com.prosilion.nostr.enums.Kind;
+import com.prosilion.nostr.event.FollowSetsEvent.EventTagAddressTagPair;
+import com.prosilion.nostr.tag.AddressTag;
+import com.prosilion.nostr.tag.EventTag;
+import com.prosilion.nostr.tag.IdentifierTag;
+import com.prosilion.nostr.user.Identity;
+import com.prosilion.nostr.user.PublicKey;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class FollowSetsEventTest {
+  Kind kind = Kind.TEXT_NOTE;
+  Identity authorIdentity = Identity.generateRandomIdentity();
+  Identity upvotedUserIdentity = Identity.generateRandomIdentity();
+  PublicKey upvotedUserPublicKey = upvotedUserIdentity.getPublicKey();
+  IdentifierTag upvote = new IdentifierTag("UPVOTE");
+  IdentifierTag downvote = new IdentifierTag("DOWNVOTE");
+  String eventId = generateRandomHex64String();
+
+  @Test
+  void testContainsFromIdenticalTags() {
+    AddressTag addressTag = new AddressTag(kind, upvotedUserPublicKey, upvote);
+    EventTag eventTag = new EventTag(eventId);
+
+    EventTagAddressTagPair eventTagAddressTagPair = new EventTagAddressTagPair(eventTag, addressTag);
+    List<EventTagAddressTagPair> eventTagAddressTagList = List.of(eventTagAddressTagPair);
+
+    EventTagAddressTagPair eventTagAddressTagPairDuplicate = new EventTagAddressTagPair(eventTag, addressTag);
+
+    List<EventTagAddressTagPair> eventTagAddressTagPairDuplicateList = List.of(eventTagAddressTagPairDuplicate);
+
+    List<EventTagAddressTagPair> nonMatches = eventTagAddressTagList.stream()
+        .filter(eventTagAddressTagPairDuplicateList::contains).toList();
+
+    assertFalse(nonMatches.isEmpty());
+
+    List<EventTagAddressTagPair> nonMatchesReverse = eventTagAddressTagPairDuplicateList.stream()
+        .filter(eventTagAddressTagPairDuplicateList::contains).toList();
+
+    assertFalse(nonMatchesReverse.isEmpty());
+  }
+
+  @Test
+  void testContainsFromDuplicateTags() {
+
+    List<EventTagAddressTagPair> eventTagAddressTagList =
+        List.of(
+            new EventTagAddressTagPair(
+                new EventTag(eventId),
+                new AddressTag(kind, upvotedUserPublicKey, upvote)));
+
+    List<EventTagAddressTagPair> eventTagAddressTagPair2 =
+        List.of(new EventTagAddressTagPair(
+            new EventTag(eventId),
+            new AddressTag(kind, upvotedUserPublicKey, upvote)));
+
+    List<EventTagAddressTagPair> nonMatches = eventTagAddressTagList.stream()
+        .filter(eventTagAddressTagPair2::contains).toList();
+
+    assertFalse(nonMatches.isEmpty());
+
+    List<EventTagAddressTagPair> nonMatchesReverse = eventTagAddressTagPair2.stream()
+        .filter(eventTagAddressTagPair2::contains).toList();
+
+    assertFalse(nonMatchesReverse.isEmpty());
+  }
+
+  public static String generateRandomHex64String() {
+    return UUID.randomUUID().toString().concat(UUID.randomUUID().toString()).replaceAll("[^A-Za-z0-9]", "");
+  }
+}
