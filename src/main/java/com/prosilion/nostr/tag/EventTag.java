3f0f4ad9c39512aef97adda82835854ef3c59f90
nick avlo <avlo@yahoo.com>
2025-10-08 17:52:40 +0800

    url validator applied to serializer

--- src/main/java/com/prosilion/nostr/tag/EventTag.java
+++ src/main/java/com/prosilion/nostr/tag/EventTag.java
@@ -5,13 +5,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.prosilion.nostr.crypto.HexStringValidator;
 import com.prosilion.nostr.enums.Marker;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import lombok.Getter;
-import org.apache.commons.lang3.stream.Streams;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -41,29 +37,23 @@ public record EventTag(
   public EventTag(@NonNull String idEvent, String recommendedRelayUrl, Marker marker) {
     this.idEvent = HexStringValidator.validateHex(idEvent, 64);
     this.marker = marker;
-
-    if (Objects.isNull(recommendedRelayUrl) || Strings.isBlank(recommendedRelayUrl)) {  // allow empty/null relayUrls...
-      this.recommendedRelayUrl = recommendedRelayUrl;
-      return;
-    }
-
-    if (LOCALHOST.matcher(recommendedRelayUrl).matches()) { // allow localhost variants
-      this.recommendedRelayUrl = recommendedRelayUrl;
-      return;
-    }
-
-    this.recommendedRelayUrl = Optional.of(recommendedRelayUrl)  // ...but validate everything else
-        .filter(s -> URL_PATTERN_W_PORT_PASSTHROUGH.matcher(s).matches())
-        .orElseThrow(() -> new InvalidUrlException(recommendedRelayUrl));
+    this.recommendedRelayUrl = urlValidator(recommendedRelayUrl);
   }
 
-  public static BaseTag deserialize(@NonNull JsonNode node) throws MalformedURLException {
-    String text = Optional.of(node.get(1)).orElseThrow().asText();
-    String recommendedRelayUrl1 = Streams.failableStream(node.get(2)).map(n -> new URI(n.asText()).toURL().toString()).stream().findFirst().orElseThrow();
-    Marker marker1 = Optional.ofNullable(node.get(3)).map(n -> Marker.valueOf(n.asText().toUpperCase())).orElse(null);
+  public static BaseTag deserialize(@NonNull JsonNode node) {
     return new EventTag(
-        text,
-        recommendedRelayUrl1,
-        marker1);
+        Optional.of(node.get(1)).orElseThrow().asText(),
+        urlValidator(Optional.ofNullable(node.get(2)).map(JsonNode::asText).orElse(null)),
+        Optional.ofNullable(node.get(3)).map(n -> Marker.valueOf(n.asText().toUpperCase())).orElse(null));
+  }
+
+  private static String urlValidator(String url) {
+    return
+        Strings.isBlank(url) || LOCALHOST.matcher(url).matches() ?
+            url
+            :
+            Optional.of(url)
+                .filter(s -> URL_PATTERN_W_PORT_PASSTHROUGH.matcher(s).matches())
+                .orElseThrow(() -> new InvalidUrlException(url));
   }
 }
