015156d18d0a934fd551bd8da22da3e361d176d5
nick avlo <avlo@yahoo.com>
2025-10-21 00:51:36 +0800

    addnl map fxns

--- src/main/java/com/prosilion/nostr/event/BadgeDefinitionReputationEventFormulaMap.java
+++ src/main/java/com/prosilion/nostr/event/BadgeDefinitionReputationEventFormulaMap.java
@@ -1,31 +1,37 @@
 package com.prosilion.nostr.event;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.util.HashMap;
+import com.prosilion.nostr.tag.IdentifierTag;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import org.springframework.lang.NonNull;
 
 public record BadgeDefinitionReputationEventFormulaMap(@NonNull BadgeDefinitionReputationEvent badgeDefinitionReputationEvent) {
   @JsonIgnore
-  public Map<BadgeDefinitionAwardEvent, List<String>> asMap() {
-    return Map.of(
-        badgeDefinitionReputationEvent,
-        badgeDefinitionReputationEvent.getArbitraryCustomAppDataFormulaEvents().stream()
-            .map(ArbitraryCustomAppDataFormulaEvent::getFormula)
-            .toList());
+  public Map<BadgeDefinitionReputationEvent, List<String>> asMap() {
+    return fxn.apply(badgeDefinitionReputationEvent);
   }
 
   @JsonIgnore
-  public Map<BadgeDefinitionAwardEvent, List<String>> asMapWithAccumulator(@NonNull BadgeDefinitionReputationEventFormulaMap other) {
-    Map<BadgeDefinitionAwardEvent, List<String>> acc = new HashMap<>(other.asMap());
-    acc.putAll(this.asMap());
-    return acc;
+  public Map<BadgeDefinitionReputationEvent, List<String>> asMapWithAccumulator(@NonNull BadgeDefinitionReputationEventFormulaMap other) {
+    asMapWithAccumulator(other.asMap());
+    return other.asMap();
   }
 
   @JsonIgnore
-  public Map<BadgeDefinitionAwardEvent, List<String>> asMapWithAccumulator(@NonNull Map<BadgeDefinitionAwardEvent, List<String>> other) {
-    other.putAll(this.asMap());
+  public Map<BadgeDefinitionReputationEvent, List<String>> asMapWithAccumulator(@NonNull Map<BadgeDefinitionReputationEvent, List<String>> other) {
+    other.putIfAbsent(badgeDefinitionReputationEvent, getUuids(badgeDefinitionReputationEvent));
     return other;
+  }
+  
+  private static final Function<BadgeDefinitionReputationEvent, Map<BadgeDefinitionReputationEvent, List<String>>> fxn =
+      event -> Map.of(event, getUuids(event));
+
+  private static List<String> getUuids(BadgeDefinitionReputationEvent badgeDefinitionReputationEvent) {
+    return badgeDefinitionReputationEvent.getArbitraryCustomAppDataFormulaEvents().stream()
+        .map(ArbitraryCustomAppDataFormulaEvent::getIdentifierTag)
+        .map(IdentifierTag::getUuid)
+        .toList();
   }
 }
