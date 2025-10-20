4b7cf3a4cb3b8e19d7b99c248f87051f1946e0a5
nick avlo <avlo@yahoo.com>
2025-10-20 23:50:26 +0800

    add ArbitraryCustomAppDataFormulaEvent parameter to BadgeDefinitionReputationEvent

--- src/main/java/com/prosilion/nostr/event/BadgeDefinitionReputationEvent.java
+++ src/main/java/com/prosilion/nostr/event/BadgeDefinitionReputationEvent.java
@@ -13,17 +13,17 @@ import org.springframework.lang.NonNull;
 
 public class BadgeDefinitionReputationEvent extends BadgeDefinitionAwardEvent {
   @JsonIgnore @Getter IdentifierTag identifierTag;
-  @JsonIgnore @Getter List<EventTag> eventTags;
+  @JsonIgnore @Getter List<ArbitraryCustomAppDataFormulaEvent> arbitraryCustomAppDataFormulaEvents;
 
   public BadgeDefinitionReputationEvent(
       @NonNull Identity identity,
       @NonNull IdentifierTag identifierTag,
-      @NonNull EventTag eventTags,
+      @NonNull ArbitraryCustomAppDataFormulaEvent arbitraryCustomAppDataFormulaEvent,
       @NonNull String content) throws NostrException {
     this(
         identity,
         identifierTag,
-        List.of(eventTags),
+        List.of(arbitraryCustomAppDataFormulaEvent),
         List.of(),
         content);
   }
@@ -31,28 +31,40 @@ public class BadgeDefinitionReputationEvent extends BadgeDefinitionAwardEvent {
   public BadgeDefinitionReputationEvent(
       @NonNull Identity identity,
       @NonNull IdentifierTag identifierTag,
-      @NonNull List<EventTag> eventTags,
+      @NonNull ArbitraryCustomAppDataFormulaEvent arbitraryCustomAppDataFormulaEvent,
+      @NonNull List<BaseTag> baseTags,
       @NonNull String content) throws NostrException {
     this(
         identity,
         identifierTag,
-        eventTags,
-        List.of(),
+        List.of(arbitraryCustomAppDataFormulaEvent),
+        baseTags,
         content);
   }
 
   public BadgeDefinitionReputationEvent(
       @NonNull Identity identity,
       @NonNull IdentifierTag identifierTag,
-      @NonNull List<EventTag> eventTags,
+      @NonNull List<ArbitraryCustomAppDataFormulaEvent> arbitraryCustomAppDataFormulaEvents,
       @NonNull List<BaseTag> baseTags,
       @NonNull String content) throws NostrException {
     super(
         identity,
         identifierTag,
-        Stream.concat(eventTags.stream(), baseTags.stream()).toList(),
+        Stream.concat(
+            arbitraryCustomAppDataFormulaEvents.stream()
+                .map(ArbitraryCustomAppDataFormulaEvent::getId)
+                .map(EventTag::new),
+            baseTags.stream()).toList(),
         content);
+    this.arbitraryCustomAppDataFormulaEvents =  arbitraryCustomAppDataFormulaEvents;
     this.identifierTag = identifierTag;
-    this.eventTags = eventTags;
+  }
+
+  @JsonIgnore
+  public List<EventTag> getEventTags() {
+    return arbitraryCustomAppDataFormulaEvents.stream()
+        .map(ArbitraryCustomAppDataFormulaEvent::getId)
+        .map(EventTag::new).toList();
   }
 }
