7c21dedae600c0194852df1c8cb79713769f4377
nick avlo <avlo@yahoo.com>
2025-11-10 18:15:37 +0800

    event tags mapped events

--- src/main/java/com/prosilion/nostr/event/EventTagsMappedEventsIF.java
+++ src/main/java/com/prosilion/nostr/event/EventTagsMappedEventsIF.java
@@ -3,12 +3,14 @@ package com.prosilion.nostr.event;
 import com.prosilion.nostr.tag.EventTag;
 import java.util.List;
 import java.util.function.Function;
+import org.springframework.lang.NonNull;
 
 public interface EventTagsMappedEventsIF {
-
-  default <T extends BaseEvent> List<T> mapEventTagEvents(BaseEvent baseEvent, Function<EventTag, T> eventTagFormulaEventFunction) {
+  default <T extends BaseEvent> List<T> mapEventTagEvents(
+      @NonNull BaseEvent baseEvent, 
+      @NonNull Function<EventTag, T> eventTagFormulaEventFxn) {
     return baseEvent.getTypeSpecificTags(EventTag.class)
         .stream()
-        .map(eventTagFormulaEventFunction).toList();
+        .map(eventTagFormulaEventFxn).toList();
   }
 }
