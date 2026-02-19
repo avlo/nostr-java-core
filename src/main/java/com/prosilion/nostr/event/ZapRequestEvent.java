4b4fe088764c228174e19739b592e43a2a0f996d
avlo <avlo@yahoo.com>
2026-02-19 16:53:42 +0800

    add zapRequest

---
+++ src/main/java/com/prosilion/nostr/event/ZapRequestEvent.java
@@ -0,0 +1,39 @@
+
+package com.prosilion.nostr.event;
+
+import com.prosilion.nostr.enums.Kind;
+import com.prosilion.nostr.event.internal.ZapRequest;
+import com.prosilion.nostr.tag.BaseTag;
+import com.prosilion.nostr.tag.GenericTag;
+import com.prosilion.nostr.tag.PubKeyTag;
+import com.prosilion.nostr.user.Identity;
+import com.prosilion.nostr.user.PublicKey;
+import java.util.List;
+import java.util.stream.Stream;
+import org.springframework.lang.NonNull;
+
+public class ZapRequestEvent extends BaseEvent {
+  public ZapRequestEvent(
+      @NonNull Identity identity,
+      @NonNull PublicKey zapReciepientPublicKey,
+      @NonNull ZapRequest zapRequest,
+      @NonNull List<BaseTag> baseTags,
+      @NonNull String content) {
+    super(
+        identity,
+        Kind.ZAP_REQUEST,
+        Stream.concat(
+            Stream.concat(
+                Stream.of(
+                    new PubKeyTag(zapReciepientPublicKey)),
+                Stream.concat(
+                    Stream.of(GenericTag.create("lnurl", zapRequest.getLnUrl())),
+                    Stream.of(GenericTag.create("amount", zapRequest.getAmount().toString())))),
+            baseTags.stream()),
+        content);
+  }
+
+  public ZapRequestEvent(@NonNull GenericEventRecord genericEventRecord) {
+    super(genericEventRecord);
+  }
+}
---
+++ src/main/java/com/prosilion/nostr/event/internal/ZapRequest.java
@@ -0,0 +1,25 @@
+package com.prosilion.nostr.event.internal;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.prosilion.nostr.tag.RelaysTag;
+import lombok.Getter;
+import org.springframework.lang.NonNull;
+
+@Getter
+public class ZapRequest {
+  @JsonProperty("relays")
+  private RelaysTag relaysTag;
+
+  @JsonProperty
+  private Long amount;
+
+  @JsonProperty("lnurl")
+  private String lnUrl;
+
+  public ZapRequest(@NonNull RelaysTag relaysTag, @NonNull Long amount, @NonNull String lnUrl) {
+    this.relaysTag = relaysTag;
+    this.amount = amount;
+    this.lnUrl = lnUrl;
+  }
+}
