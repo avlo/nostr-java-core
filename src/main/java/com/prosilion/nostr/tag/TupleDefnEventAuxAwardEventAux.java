fba63fd72396a663bdd40adaf1ba2797ae023905
nick avlo <avlo@yahoo.com>
2026-06-26 01:08:04 -0700

    tests up

--- src/main/java/com/prosilion/nostr/tag/TupleDefnEventAuxAwardEventAux.java
+++ src/main/java/com/prosilion/nostr/tag/TupleDefnEventAuxAwardEventAux.java
@@ -1,11 +1,9 @@
 package com.prosilion.nostr.tag;
 
 import com.prosilion.nostr.event.BadgeAwardGenericEventAux;
-import com.prosilion.nostr.event.BadgeDefinitionGenericEvent;
 import com.prosilion.nostr.event.BadgeDefinitionGenericEventAux;
 import com.prosilion.nostr.event.internal.Relay;
 import com.prosilion.nostr.util.Util;
-import java.util.Optional;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -21,46 +19,27 @@ public class TupleDefnEventAuxAwardEventAux extends ImmutablePair<BadgeDefinitio
      @NonNull BadgeAwardGenericEventAux badgeAwardGenericEventAux) {
     super(badgeDefinitionGenericEventAux, badgeAwardGenericEventAux);
 
-    BadgeDefinitionGenericEvent badgeDefinitionGenericEvent = badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent();
-
-    Util.debug(log, true, 'A');
-    log.debug("logging various relays");
-
-    Util.debug(log, "badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().findRelay():\n  [ {} ]",
-       badgeDefinitionGenericEvent.asAddressableEventAddressTag().findRelay().map(Relay::getUrl).orElse("EMPTY RELAY"), true, '1');
-
-    Optional<Relay> addressTagRelay = badgeDefinitionGenericEventAux.getRelay();
-
-    Util.debug(log, "badgeDefinitionGenericEventAux.getRelay():\n  [ {} ]",
-       badgeDefinitionGenericEventAux.getRelay().map(Relay::getUrl).orElse("EMPTY OPTIONAL"), true, '2');
-
-    AddressTag addressTagSupplimentalRelay = new AddressTag(
-       badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getKind(),
-       badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getPublicKey(),
-       badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getIdentifierTag(),
-       addressTagRelay.orElse(null));
-
-    Optional<RelayTag> awardRelay = badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getRelayTag();
-
     Util.debug(log, "badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getRelayTag():\n  [ {} ]",
-       badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getRelayTag().map(RelayTag::relay).map(Relay::getUrl).orElse("EMPTY"), true, '3');
-
-    Optional<Relay> awardAuxRelayTag = badgeAwardGenericEventAux.getRelay();
-
+       badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getRelayTag().map(RelayTag::relay).map(Relay::getUrl).orElse("EMPTY RELAY"), true, '1');
     Util.debug(log, "badgeAwardGenericEventAux.getRelay():\n  [ {} ]",
-       badgeAwardGenericEventAux.getRelay().map(Relay::getUrl).orElse("EMPTY OPTIONAL"), true, '4');
+       badgeAwardGenericEventAux.getRelay().map(Relay::getUrl).orElse("EMPTY RELAY"), true, '2');
 
-    Optional<Relay> eventTagRelay = awardRelay.map(RelayTag::getRelay);
-
-    Util.debug(log, true, 'B');
-
-    EventTag eventTagSupplimentalRelay = new EventTag(
-       badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getId(),
-       eventTagRelay.map(Relay::getUrl).orElse(null));
+    Util.debug(log, "badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().getRelayTag():\n  [ {} ]",
+       badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().getRelayTag().map(RelayTag::getRelay).map(Relay::getUrl).orElse("EMPTY RELAY"), true, '3');
+    Util.debug(log, "badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().findRelay():\n  [ {} ]",
+       badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().findRelay().map(Relay::getUrl).orElse("EMPTY RELAY"), true, '3');
+    Util.debug(log, "badgeDefinitionGenericEventAux.getRelay():\n  [ {} ]",
+       badgeDefinitionGenericEventAux.getRelay().map(Relay::getUrl).orElse("EMPTY RELAY"), true, '4');
 
     this.tupleATagETag = new TupleATagETag(
-       addressTagSupplimentalRelay,
-       eventTagSupplimentalRelay);
+       new AddressTag(
+          badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getKind(),
+          badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getPublicKey(),
+          badgeDefinitionGenericEventAux.getBadgeDefinitionGenericEvent().asAddressableEventAddressTag().getIdentifierTag(),
+          badgeDefinitionGenericEventAux.getRelay().orElse(null)),
+       new EventTag(
+          badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getId(),
+          badgeAwardGenericEventAux.getBadgeAwardGenericEvent().getRelayTag().map(RelayTag::getRelay).map(Relay::getUrl).orElse(null)));
   }
 
   public final String getAwardEventId() {
@@ -77,5 +56,11 @@ public class TupleDefnEventAuxAwardEventAux extends ImmutablePair<BadgeDefinitio
 
   public final Relay getDefinitionEventRelay() {
     return tupleATagETag.left.requireRelay();
+  }
+
+  public static class TupleATagETag extends ImmutablePair<AddressTag, EventTag> {
+    public TupleATagETag(@NonNull AddressTag left, @NonNull EventTag right) {
+      super(left, right);
+    }
   }
 }
